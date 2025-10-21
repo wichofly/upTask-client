1bef138d87d4c1c7281acf6c05986ac7497bd059
Mauricio Erazo <antonierazo13@hotmail.com>
2025-10-21 15:42:13 +0200

    add react-hook-form for form handling in CreateProjectView; update UI with form submission functionality and navigation link

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,19 +1,50 @@
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 export const CreateProjectView = () => {
+  const initialValues = {
+    projectName: '',
+    clientName: '',
+    description: '',
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+
+  const handleForm = (data: typeof initialValues) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <h1 className="text-5xl font-semibold">Create New Project</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">
-        Fill in the details below to create a new project.
-      </p>
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-5xl font-semibold">Create New Project</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          Fill in the details below to create a new project.
+        </p>
 
-      <Link
-        to="/"
-        className="text-white bg-purple-400 hover:bg-purple-500 px-10 py-3 text-xl font-semibold rounded-md my-5 inline-block transition-colors"
-      >
-        Back To Projects
-      </Link>
+        <Link
+          to="/"
+          className="text-white bg-purple-400 hover:bg-purple-500 px-10 py-3 text-xl font-semibold rounded-md my-5 inline-block transition-colors"
+        >
+          Back To Projects
+        </Link>
+
+        <form
+          onSubmit={handleSubmit(handleForm)}
+          noValidate
+          className="mt-10 bg-white shadow-lg p-10 rounded-md"
+        >
+          <input
+            type="submit"
+            value="Create Project"
+            className="bg-fuchsia-600 w-full p-3 text-white rounded-md uppercase font-semibold hover:bg-fuchsia-700 transition-colors cursor-pointer"
+          />
+        </form>
+      </div>
     </>
   );
 };
