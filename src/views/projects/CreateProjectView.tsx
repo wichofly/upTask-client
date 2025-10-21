51314429f92a56d89ce5a1f6af318bb78a5f1464
Mauricio Erazo <antonierazo13@hotmail.com>
2025-10-21 15:13:36 +0200

    add CreateProjectView component and update routing in Router; enhance DashboardView with project creation link

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,21 +1,19 @@
 import { Link } from 'react-router-dom';
 
-const DashboardView = () => {
+export const CreateProjectView = () => {
   return (
     <>
-      <h1 className="text-5xl font-semibold">My Projects</h1>
+      <h1 className="text-5xl font-semibold">Create New Project</h1>
       <p className="text-2xl font-light text-gray-500 mt-5">
-        Manage and track your projects efficiently.
+        Fill in the details below to create a new project.
       </p>
 
       <Link
-        to="/projects/create"
+        to="/"
         className="text-white bg-purple-400 hover:bg-purple-500 px-10 py-3 text-xl font-semibold rounded-md my-5 inline-block transition-colors"
       >
-        Create New Project
+        Back To Projects
       </Link>
     </>
   );
 };
-
-export default DashboardView;
