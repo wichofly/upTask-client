c051b45b26e66b65a1092069f872d6e843e1a62a
Mauricio Erazo <antonierazo13@hotmail.com>
2025-11-06 16:20:50 +0100

    fix: update task list header and improve task status display

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -17,6 +17,22 @@ const initialStatusGroups: GroupTasks = {
   completed: [],
 };
 
+const statusTexts: { [key: string]: string } = {
+  pending: 'Pending',
+  onHold: 'On Hold',
+  inProgress: 'In Progress',
+  underReview: 'Under Review',
+  completed: 'Completed',
+};
+
+const statusStyles: { [key: string]: string } = {
+  pending: 'border-t-slate-500',
+  onHold: 'border-t-red-500',
+  inProgress: 'border-t-blue-500',
+  underReview: 'border-t-amber-500',
+  completed: 'border-t-emerald-500',
+};
+
 export const TaskList = ({ tasks }: TaskListProps) => {
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
@@ -28,15 +44,20 @@ export const TaskList = ({ tasks }: TaskListProps) => {
 
   return (
     <>
-      <h1 className="text-5xl font-semibold my-10">Task List</h1>
+      <h1 className="text-5xl font-semibold my-10">Tasks List</h1>
 
       <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+            <h3
+              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
+            >
+              {statusTexts[status]}
+            </h3>
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">
-                  No tasks in this project
+                  There are no tasks
                 </li>
               ) : (
                 tasks.map((task) => <TaskCard key={task._id} task={task} />)
