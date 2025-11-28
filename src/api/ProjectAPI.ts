5e543c0404f1cc6e76ad08ecd0e8de1cae37a169
Mauricio Erazo <antonierazo13@hotmail.com>
2025-11-28 15:44:24 +0100

    feat: add authorization header to getProjects API call

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -18,8 +18,12 @@ export const createProject = async (formData: ProjectFormData) => {
 };
 
 export const getProjects = async () => {
+  const token = localStorage.getItem('AUTH_TOKEN'); // Retrieve token from localStorage
+  console.log(token);
   try {
-    const { data } = await api('/projects');
+    const { data } = await api('/projects', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     const response = dashboardProjectSchema.safeParse(data);
     if (response.success) {
       return response.data;
