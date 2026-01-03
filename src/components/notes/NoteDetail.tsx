3c7b613ba0ec2fea9db1327873ad1aa6b2cffce1
Mauricio Erazo <antonierazo13@hotmail.com>
2026-01-03 15:21:22 +0100

    feat: enhance NoteDetail component to include loading state and delete functionality

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from '../../hooks/useAuth';
 import type { Note } from '../../types';
 import { formatData } from '../../utils/utils';
 
@@ -6,8 +7,15 @@ type NoteDetailProps = {
 };
 
 export const NoteDetail = ({ note }: NoteDetailProps) => {
+  const { data, isLoading } = useAuth();
+
+  const canDelete = data?._id === note.createdBy._id;
+
+  if (isLoading)
+    return <p className="text-2xl text-center mt-10">Loading...</p>;
+
   return (
-    <div className="p-3 bg-gray-50 rounded-md mb-3">
+    <div className="p-3 flex justify-between items-center bg-gray-50 rounded-md mb-3">
       <div>
         <p className="text-gray-600">
           <span className="font-semibold">By {note.createdBy.name}:</span>{' '}
@@ -15,6 +23,15 @@ export const NoteDetail = ({ note }: NoteDetailProps) => {
         </p>
         <p className="text-sm text-slate-500">{formatData(note.createdAt)}</p>
       </div>
+
+      {canDelete && (
+        <button
+          type="button"
+          className="text-red-500 hover:text-red-700 hover:bg-red-50 rounded-md p-2 text-sm font-semibold cursor-pointer transition-colors"
+        >
+          Delete
+        </button>
+      )}
     </div>
   );
 };
