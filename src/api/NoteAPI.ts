41dcba76f792d184b4d488125a1d26080cfca4bf
Mauricio Erazo <antonierazo13@hotmail.com>
2026-01-03 16:03:13 +0100

    feat: implement delete functionality in NoteDetail component

--- src/api/NoteAPI.ts
+++ src/api/NoteAPI.ts
@@ -1,11 +1,12 @@
 import { isAxiosError } from 'axios';
 import api from '../lib/axios';
-import type { NoteFormData, Project, Task } from '../types';
+import type { Note, NoteFormData, Project, Task } from '../types';
 
 type NoteAPIType = {
   formData: NoteFormData;
   projectId: Project['_id'];
   taskId: Task['_id'];
+  noteId: Note['_id'];
 };
 
 export const createNote = async ({
@@ -17,6 +18,23 @@ export const createNote = async ({
     const { data } = await api.post<string>(
       `/projects/${projectId}/tasks/${taskId}/notes`,
       formData
+    );
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+};
+
+export const deleteNote = async ({
+  projectId,
+  taskId,
+  noteId,
+}: Pick<NoteAPIType, 'projectId' | 'taskId' | 'noteId'>) => {
+  try {
+    const { data } = await api.delete<string>(
+      `/projects/${projectId}/tasks/${taskId}/notes/${noteId}`
     );
     return data;
   } catch (error) {
--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,6 +1,10 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useAuth } from '../../hooks/useAuth';
 import type { Note } from '../../types';
 import { formatData } from '../../utils/utils';
+import { deleteNote } from '../../api/NoteAPI';
+import { toast } from 'react-toastify';
+import { useLocation, useParams } from 'react-router-dom';
 
 type NoteDetailProps = {
   note: Note;
@@ -9,7 +13,29 @@ type NoteDetailProps = {
 export const NoteDetail = ({ note }: NoteDetailProps) => {
   const { data, isLoading } = useAuth();
 
-  const canDelete = data?._id === note.createdBy._id;
+  const canDelete = data?._id === note.createdBy._id; // Only the creator can delete the note
+
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const taskId = queryParams.get('viewTask')!;
+
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ['task', taskId] });
+    },
+    onError: (error) => toast.error(error.message),
+  });
+
+  const handleDeleteNote = () => {
+    mutate({ projectId, taskId, noteId: note._id });
+  };
 
   if (isLoading)
     return <p className="text-2xl text-center mt-10">Loading...</p>;
@@ -28,6 +54,7 @@ export const NoteDetail = ({ note }: NoteDetailProps) => {
         <button
           type="button"
           className="text-red-500 hover:text-red-700 hover:bg-red-50 rounded-md p-2 text-sm font-semibold cursor-pointer transition-colors"
+          onClick={handleDeleteNote}
         >
           Delete
         </button>
