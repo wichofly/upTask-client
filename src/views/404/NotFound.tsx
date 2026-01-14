2631d0d53a7a9607b1d44cf165c0e6c48f822404
Mauricio Erazo <antonierazo13@hotmail.com>
2026-01-14 21:12:22 +0100

    feat: add NotFound component and route for handling 404 errors

---
+++ src/views/404/NotFound.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+export const NotFound = () => {
+  return (
+    <>
+      <h1 className="font-semibold text-center text-4xl text-white">
+        404 - Page Not Found
+      </h1>
+      <p className="mt-10 text-center text-white text-xl">
+        Go back to {''}
+        <Link className="text-fuchsia-500 " to="/">
+          Home Page
+        </Link>
+      </p>
+    </>
+  );
+};
